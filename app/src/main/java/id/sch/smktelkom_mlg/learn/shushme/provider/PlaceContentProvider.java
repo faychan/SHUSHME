bbd1ed768c56b2454ee5984b1983ecd674c35322
Fay <nor.iffat32@gmail.com>
2017-09-15 19:38:32 +0700

    -Google API Client

--- app/src/main/java/id/sch/smktelkom_mlg/learn/shushme/provider/PlaceContentProvider.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/shushme/provider/PlaceContentProvider.java
@@ -4,22 +4,6 @@ package id.sch.smktelkom_mlg.learn.shushme.provider;
  * Created by Fay on 15/09/2017.
  */
 
-/*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 import android.content.ContentProvider;
 import android.content.ContentUris;
 import android.content.ContentValues;
@@ -224,4 +208,3 @@ public class PlaceContentProvider extends ContentProvider {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
-
